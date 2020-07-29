fda752b4c47f5ab18ddc973006d8e01988d00d9a
kodingasyikid <kodingasyikid@gmail.com>
2020-07-29 16:00:13 +0700

    update php

--- coba.php
+++ coba.php
@@ -8,8 +8,20 @@
 <body>
 
    <?php 
-        echo"Hello World";
+        echo'Hello World <br> <input type="text" name="" >
+        ';
         print"Hello World";
+   ?>
+
+   <hr>
+
+   <!--  -->
+   <?php
+        $nama="Rahmad Ade Akbar"; /*string*/
+        $alamat='Banda Aceh';
+        $no_hp=6282361001252;
+
+        echo"Nama Saya Adalah :$nama <br> Alamat Saya :$alamat <br> Nomor Hp Saya : +$no_hp";
    ?>
     
 </body>
