8a5c59ab36c2277d33d22c443e5ce9c35396bb53
kodingasyikid <kodingasyikid@gmail.com>
2020-08-12 20:45:41 +0700

    update session menu

--- admin/header.php
+++ admin/header.php
@@ -221,78 +221,85 @@ session_start();
                             </button>
                         </div>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">
-                            <i class="menu-icon mdi mdi-television"></i>
-                            <span class="menu-title">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                            <i class="menu-icon mdi mdi-content-copy"></i>
-                            <span class="menu-title">Basic UI Elements</span>
-                            <i class="menu-arrow"></i>
-                        </a>
-                        <div class="collapse" id="ui-basic">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="buttons.php">Buttons</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="typography.php">Typography</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="basic_elements.php">
-                            <i class="menu-icon mdi mdi-backup-restore"></i>
-                            <span class="menu-title">Form elements</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="chartjs.php">
-                            <i class="menu-icon mdi mdi-chart-line"></i>
-                            <span class="menu-title">Charts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="basic-table.php">
-                            <i class="menu-icon mdi mdi-table"></i>
-                            <span class="menu-title">Tables</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="font-awesome.php">
-                            <i class="menu-icon mdi mdi-sticker"></i>
-                            <span class="menu-title">Icons</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                            <i class="menu-icon mdi mdi-restart"></i>
-                            <span class="menu-title">User </span>
-                            <i class="menu-arrow"></i>
-                        </a>
-                        <div class="collapse" id="auth">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="blank-page.php"> Blank Page </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="login.php"> Login </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="register.php"> Register </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="error-404.php"> 404 </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="error-500.php"> 500 </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
+                    <?php if ($_SESSION['level'] == 'admin') { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">
+                                <i class="menu-icon mdi mdi-television"></i>
+                                <span class="menu-title">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                                <i class="menu-icon mdi mdi-content-copy"></i>
+                                <span class="menu-title">Basic UI Elements</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                            <div class="collapse" id="ui-basic">
+                                <ul class="nav flex-column sub-menu">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="buttons.php">Buttons</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="typography.php">Typography</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="basic_elements.php">
+                                <i class="menu-icon mdi mdi-backup-restore"></i>
+                                <span class="menu-title">Form elements</span>
+                            </a>
+                        </li>
+
+                    <?php } else if ($_SESSION['level'] == 'operator') { ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="chartjs.php">
+                                <i class="menu-icon mdi mdi-chart-line"></i>
+                                <span class="menu-title">Charts</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="basic-table.php">
+                                <i class="menu-icon mdi mdi-table"></i>
+                                <span class="menu-title">Tables</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="font-awesome.php">
+                                <i class="menu-icon mdi mdi-sticker"></i>
+                                <span class="menu-title">Icons</span>
+                            </a>
+                        </li>
+
+                    <?php } else if ($_SESSION['level'] == 'autor') { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                                <i class="menu-icon mdi mdi-restart"></i>
+                                <span class="menu-title">User </span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                            <div class="collapse" id="auth">
+                                <ul class="nav flex-column sub-menu">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="blank-page.php"> Blank Page </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="login.php"> Login </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="register.php"> Register </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="error-404.php"> 404 </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="error-500.php"> 500 </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    <?php } ?>
                 </ul>
             </nav>
