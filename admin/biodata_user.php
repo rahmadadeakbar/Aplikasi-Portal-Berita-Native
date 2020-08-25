2c1a5ace1fc407efecbf768cd5be9b5afc4787bc
kodingasyikid <kodingasyikid@gmail.com>
2020-08-25 12:30:06 +0700

    perbaikan update biodata dengan gambar

--- admin/biodata_user.php
+++ admin/biodata_user.php
@@ -114,6 +114,8 @@ function tambah($koneksi)
                     </div>
                 </div>
 
+
+
                 <!-- function menampilkan data di table -->
                 <?php
                 function tampil_data($koneksi)
@@ -156,7 +158,7 @@ function tambah($koneksi)
                                                     </td>
 
                                                     <td>
-                                                        <a href="biodata_user.php?aksi=update&id=<?php echo $data['id_biodata']; ?>&nama=<?php echo $data['nama_kategori']; ?>" class="btn btn-warning">Edit</a>
+                                                        <a href="biodata_user.php?aksi=update&id=<?php echo $data['id_biodata']; ?>&nama=<?php echo $data['nama']; ?>&tanggal=<?php echo $data['tanggal_lahir']; ?>&tempat=<?php echo $data['tempat_lahir']; ?>&jk=<?php echo $data['jenis_kelamin']; ?>&alamat=<?php echo $data['alamat']; ?>&id_user=<?php echo $data['id_user'] ?>" class="btn btn-warning">Edit</a>
                                                         <a href="biodata_user.php?aksi=delete&id=<?php echo $data['id_biodata']; ?>" class=" btn btn-danger delete-link">Hapus</a>
                                                     </td>
                                                 </tr>
@@ -190,6 +192,140 @@ function tambah($koneksi)
 
 <?php }
             } ?>
+
+
+<?php
+function ubah($koneksi)
+{
+    if (isset($_POST['ubah_biodata'])) {
+
+        $id_biodata = $_POST['id_biodata'];
+        $id_user = $_POST['id_user'];
+        $nama = $_POST['nama_user'];
+        $tempat = $_POST['tempat_lahir'];
+        $tanggal = $_POST['tgl_lahir'];
+        $jk = $_POST['jenis_kelamin'];
+        $alamat = $_POST['alamat'];
+        $foto_sementara = !empty($_FILES['foto']['name']) ? $_FILES['foto']['name'] : '';
+        if ($foto_sementara != null) {
+
+            $foto = $foto_sementara;
+            $format = explode(".", $foto);
+            $fileExtension = end($format);
+            $nama_sementara = $_FILES['foto']['tmp_name'];
+            $md5file = md5($foto) . "." . $fileExtension;
+            $lokasi_upload = "upload/biodata/";
+            $fungsi_upload = move_uploaded_file($nama_sementara, $lokasi_upload . $md5file);
+            if ($fungsi_upload) {
+                echo '';
+            } else {
+                echo '<script>alert("gagal di upload")</script>';
+            }
+        } else {
+            $show = mysqli_query($koneksi, "SELECT * FROM biodata WHERE id_biodata='$id_biodata'");
+            $data = mysqli_fetch_array($show);
+            $md5file = $data['foto'];
+        }
+
+        // querynya
+        if (!empty($nama)) {
+            $query_update = mysqli_query($koneksi, "UPDATE biodata SET nama='$nama', tanggal_lahir='$tanggal', tempat_lahir='$tempat',jenis_kelamin='$jk',alamat='$alamat',foto='$md5file',id_user='$id_user' WHERE id_biodata='$id_biodata'");
+            if ($query_update && isset($_GET['aksi'])) {
+                if ($_GET['aksi'] == 'update') {
+                    echo '<script>alert("data berhasil di update")
+                  window.location.href="biodata_user.php";
+                </script>';
+                }
+            } else {
+                echo '<script>alert("data gagal di update")</script>';
+            }
+        }
+    }
+
+    if (isset($_GET['id'])) {
+
+?>
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Form Data Kategori</h4>
+                    <p class="card-description">
+                        Masukkan Kategori
+                    </p>
+
+                    <form class="forms-sample" action="" method="POST" enctype="multipart/form-data">
+
+                        <input type="hidden" name="id_biodata" value="<?php echo $_GET['id'] ?>">
+
+
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Pilih User</label>
+                            <?php if (!empty($_GET['id_user'])) { ?>
+                                <select class="form-control form-control-lg" id="exampleFormControlSelect1" name="id_user">
+                                    <?php
+
+                                    $id = $_GET['id_user'];
+                                    $show = mysqli_query($koneksi, "SELECT * FROM user");
+
+                                    while ($data = mysqli_fetch_array($show)) {
+
+                                    ?>
+                                        <option <?php echo ($data['id_user'] == "$id") ? 'selected' : '' ?> value="<?= $data['id_user'] ?>"><?= $data['username'] . ' - ' . $data['level'] ?></option>
+                                    <?php } ?>
+
+                                </select>
+                                <?php} else{?>
+
+                            <?php } ?>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="exampleInputName1">Nama User</label>
+                            <input type="text" class="form-control" id="exampleInputName1" placeholder="Name" name="nama_user" value="<?= $_GET['nama'] ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputName1">Tempat Lahir</label>
+                            <input type="text" class="form-control" id="exampleInputName1" placeholder="Name" name="tempat_lahir" value="<?= $_GET['tempat'] ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputName1">Tanggal Lahir</label>
+                            <input type="date" class="form-control" id="exampleInputName1" placeholder="Name" name="tgl_lahir" value="<?= $_GET['tanggal'] ?>" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputName1">Laki-Laki</label>
+                            <input type="radio" class="form-control" id="exampleInputName1" placeholder="Name" name="jenis_kelamin" value="<?php echo !empty($_GET['jk'] == "Laki-Laki") ? $_GET['jk'] : '' ?>" <?php echo !empty($_GET['jk'] == "Laki-Laki") ? 'checked' : '' ?> required>
+                            <label for="exampleInputName1">Perempuan</label>
+                            <input type="radio" class="form-control" id="exampleInputName1" placeholder="Name" name="jenis_kelamin" value="<?php echo !empty($_GET['jk'] == "Perempuan") ? $_GET['jk'] : '' ?>" <?php echo !empty($_GET['jk'] == "Perempuan") ? 'checked' : '' ?> required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputName1">Alamat</label>
+                            <textarea name="alamat" id="" class="form-control"><?= $_GET['alamat'] ?></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputName1">Foto</label>
+                            <input type="file" name="foto" class="form-control">
+                        </div>
+
+
+                        <button type="submit" class="btn btn-success mr-2" type="submit" name="ubah_biodata">Submit</button>
+                        <button class="btn btn-light" type="reset">Reset</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+
+
+<?php
+    }
+}
+
+?>
 
 
 
